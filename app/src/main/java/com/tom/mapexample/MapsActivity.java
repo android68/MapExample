46af80a2f641ea83583270e76334bccdfbef4c9e
Hank Tom <hanktom@gmail.com>
2016-08-18 15:38:22 +0800

    initial commit

---
+++ app/src/main/java/com/tom/mapexample/MapsActivity.java
@@ -0,0 +1,83 @@
+package com.tom.mapexample;
+
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import static android.Manifest.permission.*;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private static final int REQUEST_LOCATION = 100;
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}
+            , REQUEST_LOCATION);
+            Log.d("RQ", "Test");
+        }else {
+            setupMyLocation();
+        }
+        Log.d("RQ", "Test2");
+        // Add a marker in Sydney and move the camera
+        LatLng taipei101 = new LatLng(25.033684, 121.5628594);
+        mMap.addMarker(new MarkerOptions().position(taipei101).title("Marker in Taipei 101"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(taipei101, 17));
+//        mMap.moveCamera();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+            setupMyLocation();
+        }
+    }
+
+    private void setupMyLocation() {
+        //noinspection MissingPermission
+        mMap.setMyLocationEnabled(true);
+    }
+}
